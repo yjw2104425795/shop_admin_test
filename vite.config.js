ae53e2d40274f21995873e8ed6351d18e4cb1dbf
月下之夏 <yg2570496631@gmail.com>
2026-02-25 19:35:51 +0800

    配置成功代码

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +9,10 @@ export default defineConfig({
 	  vue(),
 	  tailwindcss(),
   ],
+  base:'./',
+  resolve: {
+    alias: {
+      '@':path.resolve(__dirname, 'src')
+    }
+  },
 })
